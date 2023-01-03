8c31f07ff329d72562f7f96a96eca2358f82642c
dakotaFroissart <dakota.froissart@le-campus-numerique.fr>
2023-01-03 13:57:59 +0100

    Adding various pages

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>Website Name - Homepage</title>
+    <title>Website Name</title>
 </head>
 <body>
     <header>
@@ -22,10 +22,10 @@
                         <a class="nav-link" href="nomdelapage.php">Home <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Page 2</a>
+                        <a class="nav-link" href="nomdelapage2.php">Page 2</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Page 3</a>
+                        <a class="nav-link" href="nomdelapage3.php">Page 3</a>
                     </li>
                 </ul>
             </div>
